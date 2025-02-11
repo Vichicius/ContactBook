8cf38303538609c2d65d28c1dde5be6872f5c1fd
Luis Martinez <luismartinezmilla@gmail.com>
2025-02-11 17:52:13 +0100

    Initial Commit

---
+++ ContactBook/ContactBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactBookApp.swift
+//  ContactBook
+//
+//  Created by Luis Martinez on 11/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ContactBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
